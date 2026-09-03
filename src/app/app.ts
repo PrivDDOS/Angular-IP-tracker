457ae0676297792e5448415b022f325b88d819a1
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-09-03 10:57:11 +0700

    connect the API with Typescript

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DataService } from '../data/data'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('angular-Iptracker');
+  location$!: Observable<any[]>; // The '$' suffix denotes an Observable
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.location$ = this.dataService.getLocations()
+    console.log(this.location$)
+  }
+
 }
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,6 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Data {}
+export class DataService {
+
+  private apiUrl = "https://geo.ipify.org/api/v2/country?apiKey=at_2WpoDDqzF5U588gTqHX1VzQzOWFgu&ipAddress=8.8.8.8";
+
+  constructor(private http: HttpClient) {}
+
+  getLocations(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl)
+  }
+
+}
